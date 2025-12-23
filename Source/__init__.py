cac2eb360eb68ffcb3d4408a97684006e11fe98b
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2025-12-23 12:36:38 +0200

    Refactor webhook handling to use SyncWebhook and preserve case in config options

--- Source/__init__.py
+++ Source/__init__.py
@@ -1,5 +1,6 @@
+import sys
 from configparser import ConfigParser, NoOptionError
-from discord import Webhook, RequestsWebhookAdapter
+from discord import SyncWebhook
 
 from .Utils import verify_config_section
 
@@ -12,7 +13,7 @@ except OSError:
     pass # Most likely simply means the folder already exists
 
 config = ConfigParser()
-config.optionxform = str  # Preserve case when reading config file
+config.optionxform = lambda optionstr: optionstr  # Preserve case when reading config file
 config.read("config.ini")
 
 for section in ["Webhooks", "Telegram"]:
@@ -21,6 +22,6 @@ for section in ["Webhooks", "Telegram"]:
 
 if verify_config_section(config, "Webhooks"):
     webhooks = {
-        hook_name: Webhook.from_url(hook_url, adapter=RequestsWebhookAdapter())
+        hook_name: SyncWebhook.from_url(hook_url)
         for hook_name, hook_url in config.items("Webhooks")
     }

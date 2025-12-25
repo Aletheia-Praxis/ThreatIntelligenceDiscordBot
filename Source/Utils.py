959d2579070dc7181a3f3f1860bb4a520616f396
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2025-12-25 17:11:37 +0200

    Fix access to configuration elements in the get_missing_config_params and verify_config_section functions

--- Source/Utils.py
+++ Source/Utils.py
@@ -2,13 +2,13 @@ import logging
 
 def get_missing_config_params(config, section_name):
     return [
-        detail_name for detail_name, detail in config.items(section_name) if not detail
+        detail_name for detail_name, detail in config[section_name].items() if not detail
     ]
 
 
 def verify_config_section(config, section_name):
     return section_name in config and all(
-        [detail for detail_name, detail in config.items(section_name)]
+        [detail for detail_name, detail in config[section_name].items()]
     )
 
 def configure_logger(name: str = __name__) -> logging.Logger:
